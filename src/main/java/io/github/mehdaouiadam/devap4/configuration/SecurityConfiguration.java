94fe8f772279a033f3d03d79768ef8675afd881e
mehdaouiadam <amehdaouijorge@turgot-paris.info>
2022-03-17 14:03:56 +0100

    Amélioration interface graphique + Implémentation Spring Security

---
+++ src/main/java/io/github/mehdaouiadam/devap4/configuration/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package io.github.mehdaouiadam.devap4.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    public SecurityConfiguration(){
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID");
+    }
+}
--- src/main/java/io/github/mehdaouiadam/devap4/controller/MainController.java
+++ src/main/java/io/github/mehdaouiadam/devap4/controller/MainController.java
@@ -1,17 +1,31 @@
 package io.github.mehdaouiadam.devap4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String Homepage() {
         return "Homepage";
     }
 
+    @GetMapping(path = "/login")
+    public String login(@RequestParam(name = "error", required = false) String error, Model model) {
+        // Set the "error" attribute to true if the request parameter "error" is present
+        if (error != null) {
+            model.addAttribute("error", true);
+        } else {
+            model.addAttribute("error", false);
+        }
+
+        model.addAttribute("title", "Connexion");
+        return "login";
+    }
+
     @GetMapping("/dev")
     public String Dev() { return "Dev"; }
-
 }
